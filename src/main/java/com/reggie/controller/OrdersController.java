a65cdc9348f1dc5efd47a7145d77d4ebe17b839c
WisdomTree <bigshot1221@163.com>
2023-12-04 22:40:36 +0800

    add orders list

--- src/main/java/com/reggie/controller/OrdersController.java
+++ src/main/java/com/reggie/controller/OrdersController.java
@@ -1,6 +1,8 @@
 package com.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.IdWorker;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.reggie.common.BaseContext;
 import com.reggie.common.R;
 import com.reggie.entity.AddressBook;
@@ -9,13 +11,14 @@ import com.reggie.service.AddressBookService;
 import com.reggie.service.OrdersService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.annotation.Order;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -32,6 +35,24 @@ public class OrdersController {
     public R<String> submit(@RequestBody Orders orders){
         ordersService.submit(orders);
         return R.success("下单成功");
+    }
+
+    @GetMapping("/page")
+    public R<Page<Orders>> page(int page
+            , int pageSize
+            , Long number
+            , String beginTime
+            , String endTime) {
+        Page<Orders> ordersPage = new Page<>(page, pageSize);
+
+        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(number != null, Orders::getId, number);
+
+        queryWrapper.gt(!beginTime.isEmpty(), Orders::getOrderTime, beginTime);
+        queryWrapper.lt(!endTime.isEmpty(), Orders::getOrderTime, endTime);
+
+        ordersService.page(ordersPage, queryWrapper);
+        return R.success(ordersPage);
     }
 
 }
--- src/main/java/com/reggie/controller/SetMealController.java
+++ src/main/java/com/reggie/controller/SetMealController.java
@@ -29,7 +29,7 @@ import java.util.stream.Collectors;
  */
 @Slf4j
 @RestController
-@RequestMapping("/setMeal")
+@RequestMapping("/setmeal")
 public class SetMealController {
 
     @Autowired
